2717f1f973a1018132dcda3af639d9bffc904f1f
JustLime <christian-deme@mailbox.org>
2024-01-02 20:43:18 +0100

    ci: try out windows server

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sudo chsh -s $(which zsh) $(whoami)
 zsh
+source ~/.zshrc
 
 # Install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
